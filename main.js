197338fc433e2ca83c7dd83a4ded6c4b75361640
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-05-20 19:57:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,3 +32,58 @@ fabric.Image.fromURL(get_image,function(Img)
     canvas.add(block_image_object);
 });
 }
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    keyPressed=e.keyCode;
+    console.log(keyPressed);
+    if (e.shiftKey==true && keyPressed=="80") {
+     console.log("p and shift are pressed together");
+     block_image_width=block_image_width+10;
+     block_image_height=block_image_height+10;
+     document.getElementById("current_width").innerHTML=block_image_width;
+     document.getElementById("current_height").innerHTML=block_image_height;
+    } 
+    if (e.shiftKey==true && keyPressed=="77") {
+        console.log("m and shift are pressed together");
+        block_image_width=block_image_width-10;
+        block_image_height=block_image_height-10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+       }
+       if (keyPressed=="37") {
+           Left();
+           console.log("left arrow is pressed");
+       }
+       if (keyPressed=="38") {
+        Up();
+        console.log("up arrow is pressed");
+    } 
+    if (keyPressed=="39") {
+        Right();
+        console.log("right arrow is pressed");
+    }
+    if (keyPressed=="40") {
+        Down();
+        console.log("down arrow is pressed");
+    }
+    if (keyPressed=="70") {
+        new_image("ironman_face.png");
+        console.log("f is pressed");
+    }
+    if (keyPressed=="66") {
+        new_image("spiderman_body.png");
+        console.log("b is pressed");
+    }
+    if (keyPressed=="82") {
+        new_image("thor_right_hand.png");
+        console.log("r is pressed");
+    }
+    if (keyPressed=="72") {
+        new_image("captain_america_left_hand.png");
+        console.log("h is pressed");
+    }
+    if (keyPressed=="76") {
+        new_image("hulk_legs.png");
+        console.log("l is pressed");
+    }
+}
